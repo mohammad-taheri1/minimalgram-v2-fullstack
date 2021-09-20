d399cc99c4c518051a4d3419111b234c6b0e5975
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-20 15:26:57 +0430

    working with request && response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Product List</h1>');
+    res.write('<p>Book </p>');
+
+    res.end();
 })
 
 server.listen(8000)
