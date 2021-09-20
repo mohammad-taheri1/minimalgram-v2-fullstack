051a66d2d235b3b98e85851481134174a1c6e56e
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-20 16:17:01 +0430

    add header

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
+const { clearScreenDown } = require('readline');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html')
     res.write('<h1>Product List</h1>');
     res.write('<p>Book </p>');
 
