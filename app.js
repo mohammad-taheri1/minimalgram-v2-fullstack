92b680ca8269ea30a5ee2b01c8002c9450b354e3
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-20 17:40:31 +0430

    working with express

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http')
-
 const express = require('express')
 
 const app = express()
@@ -11,8 +9,8 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log("Second middleware")
+    res.send('<h2>Product List</h2>')
 })
 
-const server = http.createServer(app)
 
-server.listen(8000)
+app.listen(8000)
