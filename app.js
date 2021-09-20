830da8126e58c21e8984919502c7a853dacddfd3
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-20 16:27:01 +0430

    routing request

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const { clearScreenDown } = require('readline');
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html')
-    res.write('<h1>Product List</h1>');
-    res.write('<p>Book </p>');
+
+    const url = req.url
+
+    if(url === '/'){
+        const form = `<form method="POST" action="/products">
+                        <input type="text" name="product" />
+                        <button type="submit">Add Product</button>
+                       </form>`
+
+        res.write(form)
+        return res.end()
+    }
+
+    const title = '<h2>Shop Page</h2>'
+    res.write(title);
+
 
     res.end();
 })
