92cb1304c415b8a4c460015951ee9be0ed292e9c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-01-19 02:35:00 +0330

    update front to show list of posts

--- frontend/front/src/App.tsx
+++ frontend/front/src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import httpClient from "./services/http.service";
+import {IPostDto} from "./types/post.types";
+import config from './utils/config.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState<IPostDto[]>([])
+
+    const getPostsFromServer = async () => {
+        const postsUrl = `${config.apiBaseUrl}/post`
+        const {data} = await httpClient.get(postsUrl);
+        setPosts(data);
+    }
+
+    useEffect(() => {
+        getPostsFromServer();
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                {
+                    posts.length > 0 && (
+                        posts.map(post => <div key={post.id}>
+                            <img src={post.image} alt={post.caption} width={300} height={300}/>
+                            <h6>{post.caption}</h6>
+                            {/*<p>{post.created_at.getUTCDate()}</p>*/}
+                        </div>)
+                    )
+                }
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/front/src/types/post.types.ts
@@ -0,0 +1,6 @@
+export interface IPostDto {
+    id: number,
+    caption: string,
+    image: string,
+    created_at: Date
+}
